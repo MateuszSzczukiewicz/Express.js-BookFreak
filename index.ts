0c8f2593377556725e2d98e9c84905c7cc2f93c3
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2023-10-22 20:04:51 +0200

    Add rate limiter

--- index.ts
+++ index.ts
@@ -5,12 +5,24 @@ import "express-async-errors";
 import { config } from "./app/config";
 import "./app/db/mongoose";
 import { bookRouter } from "./app/routes/book";
+import { handleError } from "./app/utils/errors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(
+	rateLimit({
+		windowMs: 15 * 60 * 1000,
+		limit: 100,
+		standardHeaders: "draft-7",
+		legacyHeaders: false,
+	}),
+);
 
 app.use("/api/", bookRouter);
+
+app.use(handleError);
 
 app.listen(config.port);
