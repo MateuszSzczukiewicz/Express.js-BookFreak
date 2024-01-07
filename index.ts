d4fe01dc79917d39892cd5f8acac4ff078dac590
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2024-01-07 14:06:33 +0100

    Add cors

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
+// import "express-async-errors";
 import "./app/db/mongoose";
 import { bookRouter } from "./app/routes/book";
 import { userRouter } from "./app/routes/user";
@@ -14,6 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(bodyParser.json());
+app.use(cors());
 // app.use(
 // 	rateLimit({
 // 		windowMs: 5 * 60 * 1000,
